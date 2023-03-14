545f69d898431ff853c5f78534341be5e7b5d71e
MaiiaHa <maiia.robeiko@ukr.net>
2023-03-14 23:23:02 +0100

    upd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  form: document.querySelector('#search-box'),
+};
+
+refs.form.addEventListener('input', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  console.log(e.target.value);
+}

219b1fa4df28b27c4cfc1673052970f73e9f4867
MaiiaHa <maiia.robeiko@ukr.net>
2023-03-14 22:23:33 +0000

    Deploying to gh-pages from @ MaiiaHa/goit-js-hw-10@545f69d898431ff853c5f78534341be5e7b5d71e 🚀

---
+++ index.284d0fa1.js
@@ -0,0 +1,2 @@
+({form:document.querySelector("#search-box")}).form.addEventListener("input",(function(e){e.preventDefault(),console.log(e.target.value)}));
+//# sourceMappingURL=index.284d0fa1.js.map

5e2e398c33261359e110e16f93a216e0880224f3
MaiiaHa <maiia.robeiko@ukr.net>
2023-03-16 01:32:29 +0100

    upd

---
+++ src/fetchCountries.js
@@ -0,0 +1,16 @@
+const BASE_URL = 'https://restcountries.com/v3.1/';
+
+function fetchCountries(name) {}
+return fetch(`${BASE_URL}${name}`)
+  .then(responce => {
+    return responce.json();
+    // console.log(responce.json());
+  })
+  .then(countries => {
+    console.log(countries);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -2,31 +2,49 @@ import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
+//========
+
+import debounce from 'lodash.debounce';
+import API from './fetchCountries';
+
+//========
+
 const refs = {
-  form: document.querySelector('#search-box'),
+  form: document.querySelector('s#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
-refs.form.addEventListener('input', onSubmitForm);
+refs.form.addEventListener('input', debounce(onSubmitForm, DEBOUNCE_DELAY));
 
 function onSubmitForm(e) {
   e.preventDefault();
 
-  // console.log(e.target.value);
+  const countryName = e.target.value;
+  console.log(countryName);
+
+  API.fetchCountries(countryName); // передаємо назву країни з інпут на бекенд
 }
 
-fetch(`https://restcountries.com/v3.1/name/Ukraine`)
-  .then(responce => {
-    responce.json;
-  })
-  .then(data => console.log(data));
-console.log(responce.json);
+//https://restcountries.com/v3.1/all?fields=name,capital,currencies
 
-//===============
-// fetchCountries(Ukraine);
-
-// function fetchCountries(name) {
-//   return fetch(`https://restcountries.com/v3.1/name/${name}`).then(responce => {
-//     return responce.json;
-//     console.log(responce.json);
+// const res = fetch(
+//   `https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages`
+// )
+//   .then(responce => {
+//     return responce.json();
+//     // console.log(responce.json());
+//   })
+//   .then(countries => {
+//     console.log(countries);
+//   })
+//   .catch(error => {
+//     console.log(error);
 //   });
-// }
+
+/*
+name.official - повна назва країни
+capital - столиця
+population - населення
+flags.svg - посилання на зображення прапора
+languages - масив мов */

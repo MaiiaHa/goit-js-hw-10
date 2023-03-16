e01ac1ede66fa9df007fd11684e1e0e93b756f26
MaiiaHa <maiia.robeiko@ukr.net>
2023-03-16 00:32:59 +0000

    Deploying to gh-pages from @ MaiiaHa/goit-js-hw-10@5e2e398c33261359e110e16f93a216e0880224f3 🚀

--- index.6d3a4c33.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector("#search-box")}).form.addEventListener("input",(function(e){e.preventDefault()})),fetch("https://restcountries.com/v3.1/name/Ukraine").then((e=>{e.json})).then((e=>console.log(e))),console.log(responce.json);
-//# sourceMappingURL=index.6d3a4c33.js.map
